528c713fbc867cd00020d307b19d35140e5f9411
md-zeon <zeon.cse@gmail.com>
2026-01-13 11:05:05 +0600

    fixed image break issue

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import BookShelfImage from "@/public/images/bookshelf.svg";
 
 const AuthLayout = ({
     children,
@@ -9,7 +10,7 @@ const AuthLayout = ({
         <main className="min-h-screen grid lg:grid-cols-2" suppressHydrationWarning>
             {children}
             <div className="bg-beige hidden lg:flex items-center justify-center">
-                <Image src="/images/bookshelf.svg" alt="Bookshelf" width={550} height={350} className="" />
+                <Image src={BookShelfImage} alt="Bookshelf" width={550} height={350} />
             </div>
         </main>
     );
